c2a1ae914878f0f9c9f8a2a887b63fb73d388bfb
Lucas Henrique <lucasheriques@gmail.com>
2023-05-09 16:50:11 -0300

    adding progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,60 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  large: {
+    "--padding": 4 + "px",
+    "--height": 16 + "px",
+  },
+  medium: {
+    "--padding": 0,
+    "--height": 12 + "px",
+  },
+  small: {
+    "--padding": 0,
+    "--height": 8 + "px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper style={styles}>
+      <Bar value={value} max={100} style={styles} role="progressbar">
+        <VisuallyHidden>{value}</VisuallyHidden>
+      </Bar>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: var(--padding);
+`;
+
+const Bar = styled.progress`
+  border-radius: 12px;
+  margin: 0;
+  background-color: transparent;
+  color: ${COLORS.primary};
+  border: none;
+  height: var(--height);
+  display: block;
+  width: 100%;
+
+  ::-moz-progress-bar,
+  ::-webkit-progress-bar,
+  ::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+  }
+`;
 
 export default ProgressBar;
